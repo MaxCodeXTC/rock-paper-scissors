05aeecc08b0367e24e081fd34603a3e79f8c5f85
miketheo423 <miketheo423@gmail.com>
2017-09-26 15:40:31 -0600

    Updates better pngs

--- js/app.js
+++ js/app.js
@@ -4,14 +4,18 @@ console.log('Working');
 //Changes left hand to rock
 $('body').keyup(function(e) {
 	if (e.keyCode == 65) {
-		$('#left-hand').attr('src', 'images/rock-left.png');
+		$('#left-hand')
+		.attr('src', 'images/rock-left.png')
+		.addClass('newHand');
 	}
 });
 
 //Changes left hand to paper
 $('body').keyup(function(e) {
 	if (e.keyCode == 83) {
-		$('#left-hand').attr('src', 'images/paper-left.png').addClass('newHand');
+		$('#left-hand')
+		.attr('src', 'images/paper-left.png')
+		.addClass('newHand');
 	}
 });
 
