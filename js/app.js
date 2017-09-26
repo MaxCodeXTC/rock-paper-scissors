ab944a5feae000ec0d248a5119040c7c5bae5706
miketheo423 <miketheo423@gmail.com>
2017-09-26 17:20:11 -0600

    Adds basic countdown function

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $('body').keyup(function(e) {
 	if (e.keyCode == 65) {
 		$('#left-hand')
 		.attr('src', 'images/rock-left.png')
-		.addClass('newHand');
+		.addClass('newHand rock');
 	}
 });
 
@@ -15,9 +15,64 @@ $('body').keyup(function(e) {
 	if (e.keyCode == 83) {
 		$('#left-hand')
 		.attr('src', 'images/paper-left.png')
-		.addClass('newHand');
+		.addClass('newHand')
+		.removeClass('rock');
 	}
 });
+
+//Changes left hand to scissors
+$('body').keyup(function(e) {
+	if (e.keyCode == 68) {
+		$('#left-hand')
+		.attr('src', 'images/scissors-left.png')
+		.addClass('newHand')
+		.removeClass('rock');
+	}
+});
+
+//Changes right hand to rock
+$('body').keyup(function(e) {
+	if (e.keyCode == 37) {
+		$('#right-hand')
+		.attr('src', 'images/rock-right.png')
+		.addClass('newHand rock');
+
+	}
+});
+
+//Changes right hand to paper
+$('body').keyup(function(e) {
+	if (e.keyCode == 40) {
+		$('#right-hand')
+		.attr('src', 'images/paper-right.png')
+		.addClass('newHand')
+		.removeClass('rock');
+	}
+});
+
+//Changes right hand to scissors
+$('body').keyup(function(e) {
+	if (e.keyCode == 39) {
+		$('#right-hand')
+		.attr('src', 'images/scissors-right.png')
+		.addClass('newHand')
+		.removeClass('rock');
+	}
+});
+
+function resetTimer(){
+	var sec = 3;
+}
+	$('header').on('click', 'button', function() {
+		var sec = 3;
+		var timer = setInterval(function(){
+			$('#countdown').text(sec--);
+			if(sec == -1) {
+				$('#countdown').fadeOut('fast');
+				clearInterval(timer);
+			}
+		}, 1000);
+	});
 
 
 });
