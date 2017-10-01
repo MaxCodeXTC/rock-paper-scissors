42046a6de8539cda0fe51b8a6898ae842e447dd8
miketheo423 <miketheo423@gmail.com>
2017-09-30 18:08:06 -0600

    Adds animation page and cleans up code and comments

--- js/app.js
+++ js/app.js
@@ -33,94 +33,100 @@ function startCountdown() {
 			clearInterval(timer);
 		}
 	}, 1000);
+	// Removes animation classes for hands and resets hand image
 	left.addClass('alt')
-		.removeClass('spinLeft leftLose');
+			.removeClass('spinLeft leftLose');
 	right.addClass('alt')
-		.removeClass('spinRight rightLose');
+			 .removeClass('spinRight rightLose');
 	leftHand.attr('src', '')
-			.removeClass('newHand rock paper scissors');
+					.removeClass('newHand rock paper scissors');
 	rightHand.attr('src', '')
-			.removeClass('newHand rock paper scissors');
+					 .removeClass('newHand rock paper scissors');
+	// Removes animation classes for text after win state is checked
 	$('#oneWinner, #twoWinner, #tie').removeClass('alt');
 	//Changes left hand to rock
 	$('body').keyup(function(e) {
-		if (e.keyCode == 81 && sec > 0) {
-			$('#left-hand')
-			.attr('src', 'images/rock-left.png')
-			.addClass('newHand rock')
-			.removeClass('paper scissors');
+		if (sec > 0) {
+			// Left hand rock
+			if (e.keyCode == 81) {
+				$('#left-hand')
+				.attr('src', 'images/rock-left.png')
+				.addClass('newHand rock')
+				.removeClass('paper scissors');
+			// Left hand paper
+			} else if (e.keyCode == 87) {
+				$('#left-hand')
+				.attr('src', 'images/paper-left.png')
+				.addClass('newHand paper')
+				.removeClass('rock scissors');
+			// Left hand scissors
+			} else if (e.keyCode == 69) {
+				$('#left-hand')
+				.attr('src', 'images/scissors-left.png')
+				.addClass('newHand scissors')
+				.removeClass('rock paper');
+			// Right hand rock
+	     } else if (e.keyCode == 73) {
+				$('#right-hand')
+				.attr('src', 'images/rock-right.png')
+				.addClass('newHand rock')
+				.removeClass('paper scissors');
+			// Right hand paper
+			} else if (e.keyCode == 79) {
+				$('#right-hand')
+				.attr('src', 'images/paper-right.png')
+				.addClass('newHand paper')
+				.removeClass('rock scissors');
+			// Right hand scissors
+			} else if (e.keyCode == 80)  {
+				$('#right-hand')
+				.attr('src', 'images/scissors-right.png')
+				.addClass('newHand scissors')
+				.removeClass('rock paper');
+			}
 		}
-	});
-	// Changes left hand to paper
-	$('body').keyup(function(e) {
-		if (e.keyCode == 87 && sec > 0) {
-			$('#left-hand')
-			.attr('src', 'images/paper-left.png')
-			.addClass('newHand paper')
-			.removeClass('rock scissors');
-		}
-	});
-	// Changes left hand to scissors
-	$('body').keyup(function(e) {
-		if (e.keyCode == 69 && sec > 0) {
-			$('#left-hand')
-			.attr('src', 'images/scissors-left.png')
-			.addClass('newHand scissors')
-			.removeClass('rock paper');
-		}
-	});
-	// Changes right hand to rock
-	$('body').keyup(function(e) {
-		if (e.keyCode == 73 && sec > 0) {
-			$('#right-hand')
-			.attr('src', 'images/rock-right.png')
-			.addClass('newHand rock')
-			.removeClass('paper scissors');
-		}
-	});
-	// Changes right hand to paper
-	$('body').keyup(function(e) {
-		if (e.keyCode == 79 && sec > 0) {
-			$('#right-hand')
-			.attr('src', 'images/paper-right.png')
-			.addClass('newHand paper')
-			.removeClass('rock scissors');
-		}
-	});
-	// Changes right hand to scissors
-	$('body').keyup(function(e) {
-		if (e.keyCode == 80 && sec > 0) {
-			$('#right-hand')
-			.attr('src', 'images/scissors-right.png')
-			.addClass('newHand scissors')
-			.removeClass('rock paper');
-		}
-	});
-	}
+	 });
+ }
 
 // Determines all win states and adds according animations
 function hasWon() {	
+	// Player One Win Logic
 	if (leftHand.hasClass('rock') && rightHand.hasClass('scissors') ||
-		leftHand.hasClass('paper') && rightHand.hasClass('rock') ||
-		leftHand.hasClass('scissors') && rightHand.hasClass('paper')){
+			leftHand.hasClass('paper') && rightHand.hasClass('rock') ||
+			leftHand.hasClass('scissors') && rightHand.hasClass('paper')) {
 		playerOneWin += 1;
 		round += 1;
-		left.addClass('spinLeft').removeClass('alt leftLose');
-		right.addClass('rightLose').removeClass('alt');
+
+		// Cues win animation for left hand and losing animation for right hand
+		left.addClass('spinLeft')
+				.removeClass('alt leftLose');
+
+		right.addClass('rightLose')
+				 .removeClass('alt');
+		// Cues Player One win animation
 		$('#oneWinner').addClass('alt');
+
+	// Player Two Win Logic	
 	} else if (rightHand.hasClass('rock') && leftHand.hasClass('scissors') ||
-			  	rightHand.hasClass('paper') && leftHand.hasClass('rock') ||
-				rightHand.hasClass('scissors') && leftHand.hasClass('paper')) {
+			  		 rightHand.hasClass('paper') && leftHand.hasClass('rock') ||
+						 rightHand.hasClass('scissors') && leftHand.hasClass('paper')) {
 		playerTwoWin += 1;
 		round += 1;
-		right.addClass('spinRight').removeClass('alt rightLose');
-		left.addClass('leftLose').removeClass('alt');
+
+		// Cues win animation for right hand and losing animation for left hand
+		right.addClass('spinRight')
+				 .removeClass('alt rightLose');
+		left.addClass('leftLose')
+				.removeClass('alt');
+		// Cues Player One win animation
 		$('#twoWinner').addClass('alt');
+
+	// Cues tie animation	
 	} else {
 		left.addClass('spinLeft').removeClass('alt leftLose');
 		right.addClass('spinRight').removeClass('alt rightLose');
 		$('#tie').addClass('alt');
-	}
+}
 	// Adds score depending on win state
 	$('#playerOneScore').text(playerOneWin);
 	$('#playerTwoScore').text(playerTwoWin);
@@ -131,7 +137,7 @@ function hasWon() {
 	}, 2000);
 
 	// If player gets 5 wins the page reloads
-	function winAll(){
+	function winAll() {
 		if (playerOneWin === 3) {
 			alert('Player One Wins! Click ok to restart.');
 			location.reload();
@@ -146,7 +152,7 @@ function hasWon() {
 function changeScreen() {
 	if (round === 2) {
 		$('body').css('background-image', 'url(images/space.jpg)');
-	} else if (round == 3) {
+	} else if (round === 3) {
 		$('body').css('background-image', 'url(images/Battleback_coliseum.png)');
 	} else if (round === 4) {
 		$('body').css('background-image', 'url(images/Arena_Battle_Background.jpg)');
